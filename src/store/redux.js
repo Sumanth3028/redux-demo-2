98066dafd0828f59db3d7544c1045917831649d5
Sumanth3028 <sumanthakula05@gmail.com>
2023-04-11 11:40:02 +0530

    login logout and auth redux toolkit

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,32 +1,48 @@
-import { Reducer,legacy_createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const reduce=(state={counter:0},action)=>{
-  if(action.type==='increment'){
-    return{
-        counter:state.counter+2
-    }
-  }
-  if(action.type==='decrement'){
-    return{
-        counter:state.counter-2
-    }
-  }
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuthenticated: false };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter += 2;
+    },
+    decrement(state) {
+      state.counter -= 2;
+    },
+    incrementby5(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    decrementby5(state, action) {
+      state.counter = state.counter - action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if(action.type==='incrementby5'){
-    return{
-        counter:state.counter+5
-    }
-  }
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
-  if(action.type==='decrementby5'){
-    return{
-        counter:state.counter-5
-    }
-  }
-  return state
-}
+const store = configureStore({
+  reducer: {counter:counterSlice.reducer,auth:authSlice.reducer}
+});
 
-const store=legacy_createStore(reduce)
+export const counterActions = counterSlice.actions;
+export const authActions=authSlice.actions;
 
 // const counterSubscriber=()=>{
 //     const latestState=store.getState()
@@ -37,4 +53,4 @@ const store=legacy_createStore(reduce)
 
 // store.dispatch({type:'increment'})
 
-export default store
+export default store;
